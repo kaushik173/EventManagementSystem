6e1d307217e8d4c7710ae3161d7ce3ee97b583c2
pradipgajera84 <pg@vinsha.com>
2024-07-11 09:44:00 +0530

    Domain Entities

---
+++ EventManagement.Models/Media.cs
@@ -0,0 +1,13 @@
+﻿namespace EventManagement.Models
+{
+    public class Media
+    {
+        public int MediaId { get; set; }
+        public int FestivalId { get; set; }
+        public Festival Festival { get; set; }
+        public string FileName { get; set; }
+        public string FileType { get; set; }
+        public string FilePath { get; set; }
+        public DateTime DateUploaded { get; set; }
+    }
+}
