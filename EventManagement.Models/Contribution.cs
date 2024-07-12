a59bf5424c1d857ea366c2b0291eeaa796eff1d7
Kaushik Rathod <kaushikrathod1007@gmail.com>
2024-07-12 14:31:03 +0530

    Revert "Revert "Domain Entities""
    This reverts commit e7e4de8b94e204fe4af58179c0c31cd3f0347982.

---
+++ EventManagement.Models/Contribution.cs
@@ -0,0 +1,15 @@
+﻿namespace EventManagement.Models
+{
+    public class Contribution
+    {
+        public int ContributionId { get; set; }
+        public int? MemberId { get; set; }
+        public Member Member { get; set; }
+        public int? ContributorId { get; set; }
+        public Contributor Contributor { get; set; }
+        public int FestivalId { get; set; }
+        public Festival Festival { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ EventManagement.Models/Expense.cs
@@ -0,0 +1,12 @@
+﻿namespace EventManagement.Models
+{
+    public class Expense
+    {
+        public int ExpenseId { get; set; }
+        public int FestivalId { get; set; }
+        public Festival Festival { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
